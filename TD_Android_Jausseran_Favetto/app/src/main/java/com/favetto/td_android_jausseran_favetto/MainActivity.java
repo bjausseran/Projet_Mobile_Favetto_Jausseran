683bde2f19cda45aa1a253512dd9325017e2a5cd
bjausseran <benoit.jausseran@ynov.com>
2020-11-24 18:27:44 +0100

    first commit bdd/networking/

---
+++ TD_Android_Jausseran_Favetto/app/src/main/java/com/favetto/td_android_jausseran_favetto/MainActivity.java
@@ -0,0 +1,14 @@
+package com.favetto.td_android_jausseran_favetto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
