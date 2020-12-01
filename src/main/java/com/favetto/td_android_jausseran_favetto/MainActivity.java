489d0e35b545cfccb641b26b12d4dfcbfb315e3a
AmbreFavetto <47179688+AmbreFavetto@users.noreply.github.com>
2020-12-01 09:46:38 +0100

    Morpion local
    Code du morpion fonctionnel en local

--- src/main/java/com/favetto/td_android_jausseran_favetto/MainActivity.java
+++ src/main/java/com/favetto/td_android_jausseran_favetto/MainActivity.java
@@ -1,8 +1,10 @@
 package com.favetto.td_android_jausseran_favetto;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -113,6 +115,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             currentPlayer = 1;
             txtPlayer.setText("X");
+        }
+        int res = checkWinner();
+        displayAlertDialog(res);
+    }
+
+    // 0 : partie non fini
+    // 1 : X
+    // 2 : O
+    // 3 : egalite
+    private int checkWinner(){
+
+        // on regarde si il y a un gagnant sur les colonnes
+        for (int col = 0; col <= 2; col++) {
+            if (boardGame[col][0] != 0 && boardGame[col][0] == boardGame[col][1] && boardGame[col][0] == boardGame[col][2])
+                return boardGame[col][0];
+        }
+
+        // on regarde si il y a un gagnant sur les lignes
+        for (int line = 0; line <= 2; line++){
+            if (boardGame[0][line] != 0 && boardGame[0][line] == boardGame[1][line] && boardGame[0][line] == boardGame[2][line])
+                return boardGame[0][line];
+        }
+
+        // on regarde si il y a un gagnant sur la diagonale haut/gauche -> bas/droit
+        if (boardGame[0][0] != 0 && boardGame[0][0] == boardGame[1][1] && boardGame[0][0] == boardGame[2][2])
+            return boardGame[0][0];
+
+        // on regarde si il y a un gagnant sur la diagonale haut/droite -> bas/gauche
+        if (boardGame[2][0] != 0 && boardGame[2][0] == boardGame[1][1] && boardGame[2][0] == boardGame[0][2])
+            return boardGame[2][0];
+
+        // Egalité
+        boolean isFull = true;
+        for (int col = 0; col <= 2; col++) {
+            for (int line = 0; line <= 2; line++){
+                if (boardGame[col][line] == 0) { // case
+                    isFull = false;
+                    break;
+                }
+            }
+            if (!isFull)
+                break;
+        }
+        if (isFull)
+            return 3;
+
+        // Partie non fini
+        return 0;
+    }
+
+    // 0 : partie non fini
+    // 1 : X
+    // 2 : O
+    // 3 : egalite
+    private void displayAlertDialog(int res){
+        if (res == 0) // partie non termine
+            return;
+
+        String strToDisplay = "";
+        if (res == 1)
+            strToDisplay = "Les X ont gagnées !";
+        if (res == 2)
+            strToDisplay = "Les O ont gagnés !";
+        if (res == 3)
+            strToDisplay = "Egalité !";
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Fin de la partie");
+        alertDialog.setMessage(strToDisplay);
+
+        alertDialog.setNeutralButton("Recommencer", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                resetGame();
+            }
+        });
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+
+    }
+
+    private void resetGame(){
+
+        for (int col = 0; col <= 2; col++) {
+            for (int line = 0; line <= 2; line++) {
+                boardGame[col][line] = 0;
+            }
+        }
+
+        for (Button bt : all_buttons) {
+            //bt.setBackground(null); //Si API >= 16
+            bt.setBackground(null);
         }
     }
 }
