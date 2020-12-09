51e14c6f922859378f2c85fafb475058c04ded28
bjausseran <benoit.jausseran@ynov.com>
2020-12-09 22:33:11 +0100

    nbPlayers + currPlayer

--- TD_Android_Jausseran_Favetto/app/src/main/java/com/favetto/td_android_jausseran_favetto/MainActivity.java
+++ TD_Android_Jausseran_Favetto/app/src/main/java/com/favetto/td_android_jausseran_favetto/MainActivity.java
@@ -24,35 +24,30 @@ public class MainActivity extends AppCompatActivity {
 
     String[] letters =new String[]{"a","b","c"};
     private int boardGame[][] = new int[3][3];
+    private boolean hasStop = false;
 
-    // 1 : X
-    // 2 : O
+    // Joueur actuel    1 : X
+    //                  2 : O
     private int currentPlayer = 1;
+    // Numéro du joueurs
     private int isPlayer = 0;
+    // Nombre de joueurs en ligne
+    private int nbPlayer = 0;
 
-    private TextView txtPlayer;
+    private TextView txtCurrPlayer;
     private TextView txtIsPlayer;
+    private TextView txtInGame;
 
     private ArrayList<Button> all_buttons = new ArrayList<>();
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRefNb = database.getReference("nbPlayers");
-        myRefNb.setValue(isPlayer - 1);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtPlayer = findViewById(R.id.player);
+        txtCurrPlayer = findViewById(R.id.player);
         txtIsPlayer = findViewById(R.id.isPlayer);
-
-        //Clear button (here for tests)
-        Button btnClear = findViewById(R.id.buttonClear);
+        txtInGame = findViewById(R.id.inGame);
 
         // Find how many players are connected
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -60,21 +55,79 @@ public class MainActivity extends AppCompatActivity {
         final DatabaseReference myRefcurr = database.getReference("currPlayer");
 
 
+    //_________________________________JOUEUR ACTUEL_________________________//
+        // Recupère le joueur actuel à chaque update de currPlayer dans Firebase
+        myRefcurr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String place = dataSnapshot.getKey();
+                Integer value = dataSnapshot.getValue(Integer.class);
+                currentPlayer = value;
+                //Changement de joueur
+                if (currentPlayer == 1) {
+                    txtCurrPlayer.setText("X");
+                } else {
+                    currentPlayer = 2;
+                    txtCurrPlayer.setText("O");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("APPX", "Failed to read value", error.toException());
+            }
+        });
+    //____________________________________________________________________________//
+    //_________________________________NOMBRE DE JOUEUR_________________________//
+        // Tourne une fois sur nbPLayers, pour récupérer le numero du joueurs
+        myRefNb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer value = dataSnapshot.getValue(Integer.class);
+                myRefNb.setValue(value + 1);
+                isPlayer = value + 1;
+                nbPlayer = value + 1;
 
+                CheckEnoughPlayer();
+                SetPlayerText(value + 1);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("APPX", "Failed to read value", error.toException());
+            }
+        });
 
+        // Tourne à chaque fois que le valeur nbPlayers change dans Firebase
+        myRefNb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer value = dataSnapshot.getValue(Integer.class);
+                nbPlayer = value;
+                CheckEnoughPlayer();
+                //Si    numero joueur > nombre joueurs
+                //      numero joueur = nombre joueurs = dernier joueur
+                if (isPlayer > isPlayer) {
+                    SetPlayerText(isPlayer);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("APPX", "Failed to read value", error.toException());
+            }
+        });
+    //____________________________________________________________________________//
+    //___________________________________FOR TEST_________________________________//
+        //Clear button (here for tests)
+        Button btnClear = findViewById(R.id.buttonClear);
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                for (int i = 1; i < 4; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        DatabaseReference myRef = database.getReference(letters[j] + i);
-                        myRef.setValue("");
-                    }
-                }
+                ClearTable();
             }
         });
-                // Set all button of the grid
+
+    //____________________________________________________________________________//
+    //______________________________GRID BUTTON SETTER____________________________//
+        // Set all button of the grid
         for (int i = 1; i < 4; i++) {
             for (int j = 0; j < 3; j++) {
                 int id = getResources().getIdentifier("btn_" + letters[j] + i, "id", getPackageName());
@@ -86,92 +139,29 @@ public class MainActivity extends AppCompatActivity {
                 Button button = findViewById(id);
                 //bt.setBackground(null); //Si API >= 16
                 button.setBackground(null);
+
+        //------------------------------ON CLICK LISTENER-------------------------//
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
 
                         //On ne fait rien si la case cliqué n'est pas vide
-                        if (view.getBackground() != null)
-                            return;
-                        if (currentPlayer != isPlayer)
+                        if (view.getBackground() != null || currentPlayer != isPlayer || nbPlayer < 2)
                             return;
 
                         FirebaseDatabase database = FirebaseDatabase.getInstance();
                         DatabaseReference myRef = database.getReference(letter + num);
-                        myRef.setValue(Integer.toString(currentPlayer));
+                        myRef.setValue(Integer.toString(isPlayer));
 
                         //Changement de joueur
-                        if (isPlayer == 1) {
-                            myRefcurr.setValue(2);
-                        } else {
-                            myRefcurr.setValue(1);
-                        }
+                        if (isPlayer == 1) myRefcurr.setValue(2);
+                        else myRefcurr.setValue(1);
                     }
                 });
 
-                // Run everytime time nbPlayers value is updated
-                myRefcurr.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String place = dataSnapshot.getKey();
-                        Integer value = dataSnapshot.getValue(Integer.class);
-                        currentPlayer = value;
-                        //Changement de joueur
-                        if (value == 1) {
-                            currentPlayer = 1;
-                            txtPlayer.setText("O");
-                        } else {
-                            currentPlayer = 2;
-                            txtPlayer.setText("X");
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.w("APPX", "Failed to read value", error.toException());
-                    }
-                });
-
+        //---------------------------FIREBASE READING-------------------------//
                 DatabaseReference myRef = database.getReference(letter + num);
-
-                // Run one time on nbPlayers value
-                myRefNb.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String place = dataSnapshot.getKey();
-                        Integer value = dataSnapshot.getValue(Integer.class);
-                        myRefNb.setValue(value + 1);
-                        isPlayer = value +1;
-                        if (value + 1 == 1) {
-                            txtIsPlayer.setText("X");
-                        } else {
-                            txtIsPlayer.setText("O");
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.w("APPX", "Failed to read value", error.toException());
-                    }
-                });
-                // Run everytime time nbPlayers value is updated
-                myRefNb.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String place = dataSnapshot.getKey();
-                        Integer value = dataSnapshot.getValue(Integer.class);
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.w("APPX", "Failed to read value", error.toException());
-                    }
-                });
-
-                // Run everytime a firebase case value is updated
+                // Run everytime a firebase case is updated
                 myRef.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -207,6 +197,82 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
+        }
+    }
+
+    //OnPause retire le joueur de decompte Firebase
+    //   Si c'est le derneir joueur : clear table + reset first player
+    @Override
+    protected void onPause() {
+        super.onPause();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRefNb = database.getReference("nbPlayers");
+        myRefNb.setValue(nbPlayer - 1);
+        if (nbPlayer == 1)
+        {
+            final DatabaseReference myRefCurr = database.getReference("currPlayer");
+            myRefCurr.setValue(1);
+            ClearTable();
+        }
+        hasStop = true;
+    }
+    //OnPause retire le joueur de decompte Firebase
+    //   Si c'est le derneir joueur : clear table + reset first player
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (hasStop) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            final DatabaseReference myRefNb = database.getReference("nbPlayers");
+            myRefNb.setValue(nbPlayer + 1);
+            isPlayer = nbPlayer +1;
+            if (nbPlayer >= 2) {
+                final DatabaseReference myRefCurr = database.getReference("currPlayer");
+                myRefCurr.setValue(1);
+                ClearTable();
+            }
+            hasStop = false;
+        }
+    }
+
+    // Vide la table Firebase
+    private void ClearTable()
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        for (int i = 1; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                DatabaseReference myRef = database.getReference(letters[j] + i);
+                myRef.setValue("");
+            }
+        }
+    }
+
+    // Verifie s'il y a assez de joueur et prépare la partie
+    private void CheckEnoughPlayer()
+    {
+        if (nbPlayer >= 2)
+        {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            final DatabaseReference myRefCurr = database.getReference("currPlayer");
+            myRefCurr.setValue(1);
+            ClearTable();
+
+            if (isPlayer <= 2) txtInGame.setText("En partie");
+            else txtInGame.setText("En attente");
+        }
+        else txtInGame.setText("En attente");
+    }
+
+    private void SetPlayerText(int value) {
+        if (value == 1) {
+            txtIsPlayer.setText("X");
+            txtIsPlayer.setTextSize(30);
+        }else if(value == 2){
+            txtIsPlayer.setText("O");
+            txtIsPlayer.setTextSize(30);
+        } else {
+            txtIsPlayer.setText("NONE");
+            txtIsPlayer.setTextSize(10);
         }
     }
 }
